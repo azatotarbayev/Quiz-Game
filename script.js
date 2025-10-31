65d2098b1c3df51b3e733c2846c68f04084c5dc4
Azat Otarbayev <nfs.lover.1991@gmail.com>
2025-11-01 01:57:11 +0500

    Finished a project!

--- script.js
+++ script.js
@@ -26,38 +26,38 @@ const quizQuestions = [
     {
         question: "Which planet is known as the Red Planet?",
         answers: [
-        { text: "Venus", correct: false },
-        { text: "Mars", correct: true },
-        { text: "Jupiter", correct: false },
-        { text: "Saturn", correct: false },
-        ],
+            { text: "Venus", correct: false },
+            { text: "Mars", correct: true },
+            { text: "Jupiter", correct: false },
+            { text: "Saturn", correct: false }
+        ]
     },
     {
         question: "What is the largest ocean on Earth?",
         answers: [
-        { text: "Atlantic Ocean", correct: false },
-        { text: "Indian Ocean", correct: false },
-        { text: "Arctic Ocean", correct: false },
-        { text: "Pacific Ocean", correct: true },
-        ],
+            { text: "Atlantic Ocean", correct: false },
+            { text: "Indian Ocean", correct: false },
+            { text: "Arctic Ocean", correct: false },
+            { text: "Pacific Ocean", correct: true }
+        ]
     },
     {
         question: "Which of these is NOT a programming language?",
         answers: [
-        { text: "Java", correct: false },
-        { text: "Python", correct: false },
-        { text: "Banana", correct: true },
-        { text: "JavaScript", correct: false },
-        ],
+            { text: "Java", correct: false },
+            { text: "Python", correct: false },
+            { text: "Banana", correct: true },
+            { text: "JavaScript", correct: false }
+        ]
     },
     {
         question: "What is the chemical symbol for gold?",
         answers: [
-        { text: "Go", correct: false },
-        { text: "Gd", correct: false },
-        { text: "Au", correct: true },
-        { text: "Ag", correct: false },
-        ],
+            { text: "Go", correct: false },
+            { text: "Gd", correct: false },
+            { text: "Au", correct: true },
+            { text: "Ag", correct: false }
+        ]
     }
 ];
 
@@ -66,8 +66,8 @@ let currentQuestionIndex = 0;
 let score = 0;
 let answerDisabled = false;
 
-totalQuestionsSpan.textContent = quizQuestions.length;
-maxScoreSpan.textContent = quizQuestions.length;
+totalQuestionsSpan.textContent = quizQuestions.length; // Question 1 of {5}
+maxScoreSpan.textContent = quizQuestions.length; // You score 0 out of {5}
 
 //event listeners
 startButton.addEventListener("click", startQuiz);
@@ -77,7 +77,8 @@ function startQuiz()
 {
     // reset vars
     currentQuestionIndex = 0;
-    scoreSpan.textContent = 0;
+    score = 0;
+    scoreSpan.textContent = 0; // Score: 0
 
     startScreen.classList.remove("active");
     quizScreen.classList.add("active");
@@ -89,13 +90,98 @@ function showQuestion()
 {
     answerDisabled = false;
 
+    // установить вопрос
     const currentQuestion = quizQuestions[currentQuestionIndex];
     currentQuestionSpan.textContent = currentQuestionIndex + 1;
 
+    // установить прогресс
     const progressPercent = (currentQuestionIndex / quizQuestions.length) * 100;
+    progressBar.style.width = progressPercent + "%";
+
+    // получить вопрос из массива объектов (вопрос-ответ)
+    questionText.textContent = currentQuestion.question;
+
+    // очистить контейнер ответов (чтобы ответы не накладывались друг на друга)
+    answersContainer.innerHTML = "";
+
+    // пройтись по массиву ответов (добавить 4 ответа в блок)
+    // [].foreach(func())
+    currentQuestion.answers.forEach(answerObject => {
+        // создать кнопку
+        const button = document.createElement("button");
+        button.textContent = answerObject.text; // вариант ответа
+        button.classList.add("answer-btn");
+        button.dataset.correct = answerObject.correct;
+
+        button.addEventListener("click", selectAnswer);
+
+        answersContainer.appendChild(button);
+    });
+}
+
+function selectAnswer(event)
+{
+    if (answerDisabled)
+        return;
+
+    answerDisabled = true;
+
+    // нажатая кнопка
+    const selectedButton = event.target;
+    const isCorrect = selectedButton.dataset.correct === "true";
+    
+    // array.from - creates an array btw
+    Array.from(answersContainer.children).forEach((button) => {
+        if (button.dataset.correct === "true")
+            button.classList.add("correct");
+        else if (button === selectedButton)
+            button.classList.add("incorrect");
+    });
+
+    // updating a user's score
+    if (isCorrect)
+    {
+        score++;
+        scoreSpan.textContent = score;
+    }
+
+    setTimeout(() => {
+        currentQuestionIndex++;
+
+        // check if there are more questions
+        // OR if the quiz is over
+        if (currentQuestionIndex < quizQuestions.length)
+            showQuestion();
+        else
+            showResults();
+    }, 1000);
+}
+
+function showResults()
+{
+    quizScreen.classList.remove("active");
+    resultScreen.classList.add("active");
+
+    finalScoreSpan.textContent = score;
+    const percentage = (score/quizQuestions.length) * 100;
+
+    if (percentage === 100)
+        resultMessage.textContent = "Perfect! You're a genius!";
+    else if (percentage >= 80)
+        resultMessage.textContent = "Great job! You know your stuff!";
+    else if (percentage >= 60)
+        resultMessage.textContent = "Good effort! Keep learning!";
+    else if (percentage >= 40)
+        resultMessage.textContent = "Not bad! Try again to improve!";
+    else
+        resultMessage.textContent = "Keep studying! You'll get better!";
 }
 
 function restartQuiz()
 {
     console.log("quiz restarted");
+
+    resultScreen.classList.remove("active");
+
+    startQuiz();
 }
